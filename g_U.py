6ef3704721ea959cee68edf326873aa2d07cdc2c
mercan ölçek <221722595+mercan798@users.noreply.github.com>
2026-04-03 18:29:28 +0300

    Enhance UI by adding CSS and adjusting textbox lines

--- g_U.py
+++ g_U.py
@@ -3,23 +3,25 @@ from weather_api import get_weather
 from currency_api import currency
 from crypto_api import crypto
 
+css = open("style.css").read()
+
 with gr.Blocks() as demo:
     gr.Markdown("# Weather, Currency and Crypto Info App")
 
     with gr.Tab("Weather Info"):
         city = gr.Textbox(label="Enter City Name")
         button = gr.Button("Get Weather Info")
-        output = gr.Textbox(label="Weather Information")
+        output = gr.Textbox(label="Weather Information", lines=5)
         button.click(fn=get_weather, inputs=city, outputs=output)
 
     with gr.Tab("Currency Info"):
         button2 = gr.Button("Get Currency Info")
-        output2 = gr.Textbox(label="Currency Information")
+        output2 = gr.Textbox(label="Currency Information", lines=5)
         button2.click(fn=currency, inputs=None, outputs=output2)
 
     with gr.Tab("Crypto Info"):
         button3 = gr.Button("Get Crypto Info")
-        output3 = gr.Textbox(label="Crypto Information")
+        output3 = gr.Textbox(label="Crypto Information", lines=5)
         button3.click(fn=crypto, inputs=None, outputs=output3)
 
-demo.launch(share=True)
+demo.launch(css=css, share=True, footer_links=[], inbrowser=True, show_error=True)
